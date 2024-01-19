6cc99e1219699fe42ac5bc616c288d6ece38b0ef
Callum Chaney <35102876+CallumChaney@users.noreply.github.com>
2024-01-19 12:39:38 +0000

    Basic single threaded http server

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ use std::{
 const HOST: &str = "127.0.0.1";
 const PORT: &str = "7878";
 
+const GET_INDEX: &str = "GET / HTTP/1.1";
+
+const STATUS_200_OK: &str = "HTTP/1.1 200 OK";
+const STATUS_400_NOTFOUND: &str = "HTTP/1.1 404 NOT FOUND";
+
+enum RequestLine {
+    GET(String),
+}
+
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind(format!("{}:{}", HOST, PORT))
         .expect(&format!("Could not connect to server at {}:{}", HOST, PORT));
@@ -23,14 +32,15 @@ fn main() -> std::io::Result<()> {
 
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let buf_reader = BufReader::new(&mut stream);
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap()?;
 
-    let status_line = "HTTP:/1.1 200 OK";
-    let contents = fs::read_to_string("index.html")?;
+    let (status_line, file_name) = if request_line == GET_INDEX {
+        (STATUS_200_OK, "index.html")
+    } else {
+        (STATUS_400_NOTFOUND, "404.html")
+    };
+
+    let contents = fs::read_to_string(file_name)?;
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes())?;
